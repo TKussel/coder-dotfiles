8a1446618ca644359c580dd65de038df16d86777
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2026-06-24 07:35:04 +0200

    Bump manual nvim version

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-curl -LO https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/download/v0.12.3/nvim-linux-x86_64.tar.gz
 #curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo tar -C /home/coder/bin -xzf nvim-linux-x86_64.tar.gz
 echo 'export PATH="$PATH:~/bin/nvim-linux-x86_64/bin"' | tee /home/coder/.bashrc | tee /home/coder/.zshrc
